09e103e4ea676bedfec7b0a113ece923b8e8fd80
fernandoG494 <lfgc851@gmail.com>
2022-06-30 10:14:49 -0500

    blocks recap

---
+++ Javascript recap/blocks.js
@@ -0,0 +1,14 @@
+const name1 = 'Fer';
+
+{
+    const name2 = 'Fernando';
+    console.log(name2);
+    console.log(name1);
+}
+
+{
+    const name = 'dev';
+    console.log(name);
+}
+
+console.log(name2);
